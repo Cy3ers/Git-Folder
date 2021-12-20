1fe9fbabf3d42c3afd8e48ebf05cb3bcce720fc5
Faizaan <faizaan.talha@gmail.com>
2021-12-20 16:09:54 +0500

    First Add

---
+++ 2020120_Lab9_Task1.py
@@ -0,0 +1,39 @@
+def dcprt(text,s):
+	result = ""
+	for i in range(len(text)):
+		char = text[i]
+
+		if (char.isupper()):
+			result = result + chr((ord(char) - s-65) % 26 + 65)
+		else:
+			result = result + chr((ord(char) - s - 97) % 26 + 97)
+
+	return result
+
+def encrpt(text,s):
+	result = ""
+	for i in range(len(text)):
+		char = text[i]
+
+		if (char.isupper()):
+			result += chr((ord(char) + s-65) % 26 + 65)
+		else:
+			result += chr((ord(char) + s - 97) % 26 + 97)
+
+	return result
+
+
+text = "DEFENDTHEFORT"
+s = 7
+print ("\n")
+
+enc = encrpt(text,s)
+dec = dcprt(enc,s)
+
+print (f"Original Text: {text}")
+print (f"Shift : {s}")
+print (f"Encrypted Text: {enc}")
+print ("\n")
+print (f"Encrypted Text : {enc}")
+print (f"Shift : {s}")
+print (f"Original Text: {dec}")
